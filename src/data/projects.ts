f5b99ab46d8635da20c6fc2a8f4cedc74ef801a4
ds4g24 <ds4g24@soton.ac.uk>
2026-06-30 16:15:34 +0100

    Added Apexle Project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -8,7 +8,7 @@ export const PROJECTS_DATA: Project[] = [
     description:
       "A game made by me and a team for GGJ26. It won the Players Choice Award for Southampton.",
     image: "/images/projects/damaskus-cover.jpg",
-    tags: ["Godot", "Git", "Puzzle Design", "Game Jam"],
+    tags: ["Godot", "Puzzle Design", "Game Jam", "Git"],
     featured: true,
     featuredOrder: 1,
     inDevelopment: false,
@@ -19,6 +19,23 @@ export const PROJECTS_DATA: Project[] = [
     links: {
       play: "https://damaskus.indigo.spot",
       github: "https://github.com/Lem0naise/damaskus",
+    },
+  },
+  {
+      id: "project2",
+    title: "Apexle",
+    subtext: "Daily F1 Game",
+    description:
+      "Using real telemetry data, you guess the corner of an F1 track, in a wordle-style daily game.",
+    image: "/images/projects/apexle-cover.jpg",
+    imageFocus: "top",
+    tags: ["Website", "UI/UX Design"],
+    featured: false,
+    featuredOrder: 1,
+    inDevelopment: false,
+    links: {
+      play: "https://apexle.dexo.games",
+      github: "https://github.com/DexoGames/Apexle",
     },
   },
 ];
